4b73d60c57eec0de489eeccbeef4aee3960c1c35
Techcable <Techcable@techcable.net>
2021-06-14 17:18:42 -0700

    Ch 1: Explain Example 1.5 "normal" vs "applicative" order
    Java vs Haskell, who wins?
    Java obviously :p

--- excercises/chapter1.scm
+++ excercises/chapter1.scm
@@ -33,3 +33,34 @@
 ;; This effectively adds the absolute value of b,
 ;; because the sign of b is ignored and its magnitude
 ;; is always used to increase a (I tried to describe it without using that word though).
+
+
+
+;; **** Exercise 1.5 ****
+;; Ben Bitwiddle invents the following test to test whether the interpreter
+;; uses "normal order" or "applicative order" evaluation
+;;
+;; To review (for my sake), "applicative order" means to evaluate the args first,
+;; and then apply. In other words, it's "eager" evaluation, like in Java or C.
+;; The alternative "fully expand and then reduce" is called "normal order".
+;; In other words it's "lazy" evaluation, like in Haskell (I think).
+(define (p) (p))
+(define (test x y) (if (= x 0) 0 y))
+
+;; NOTE: P evaluates by calling itself.
+;; In "applicative order" mode,
+;; this causes infinite recursion.
+;; Because p() will call itself and loop infinitely.
+;; At first I thought this might create some sort of closure
+;; or function reference,
+;; but it does in fact do the stupid thing
+;; and perform a call (and thus loop forever)
+;; In "normal order" mode, the if test evaluates
+;; to true, and the `p` function doesn't need to be
+;; evaluated (because everything is lazy).
+;; The result is `0` instead of an
+;; infinite loop.
+;; I guess the author is trying to hint at the
+;; advantage of "normal order"/lazy evaluated languages.
+;; However, I'm still skeptical it is worth the complexity
+;; (despite not seriously working with it much) :p
